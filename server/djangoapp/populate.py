157045b33bb289f6c211d70c81cf0f9f3b93b158
arpitSharma-2003 <113616660+arpitSharma-2003@users.noreply.github.com>
2026-05-07 20:15:16 +0530

    Fix indentation in populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -63,7 +63,8 @@ def initiate():
         },
         {
             "name": "Carnival", "type": "SUV", "year": 2023,
-            "car_make": car_make_instances[3]},
+            "car_make": car_make_instances[3]
+        },
         {
             "name": "Cerato", "type": "Sedan", "year": 2023,
             "car_make": car_make_instances[3]
